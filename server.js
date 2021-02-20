f59af9d029fa4b0353d611b6a3bd1d4ed0208e26
Francesco <franci.ducci@gmail.com>
2021-02-20 17:40:34 +0100

    aggiunta richiesta openweather

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
+const got = require('got');
+var key = "aca959ae6d5053b35c1f3e2f551b0b49";
 
-app.get("/luogo",(req,resp) =>{
-    resp.send("Ciao!");
+app.get("/luogo",async(req,resp) =>{
+    const città = req.query.città;
+    const meteo = await got('api.openweathermap.org/data/2.5/weather?q='+città'&appid='+key);
+    const tempo = meteo.body;
+    resp.send(tempo);
     resp.sendStatus(200);
 })
 
