f76cfe22e20f130c1635bc9c6bb74ccf5e0626c2
Francesco <48328557+Francy9@users.noreply.github.com>
2021-02-21 00:32:32 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const app = express();
 const got = require('got');
 var key = "aca959ae6d5053b35c1f3e2f551b0b49";
 
-app.get("/luogo/:città",async(req,resp) =>{
-   const città = req.params.città;
+app.get("/luogo/:nome",async(req,resp) =>{
+   const nome = req.params.nome;
    console.log(città);
     try {
-	const response = await got('http://api.openweathermap.org/data/2.5/weather?q='+ città +'&appid='+ key +'&mode=json&units=metric&lang=it');
+	const response = await got('http://api.openweathermap.org/data/2.5/weather?q='+ nome +'&appid='+ key +'&mode=json&units=metric&lang=it');
         const corpo = JSON.parse(response.body);
         resp.send(corpo);
 	} catch (error) {
