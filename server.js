948e51d8ce4f273832128d854130d71ddfe8e8db
Francesco <franci.ducci@gmail.com>
2021-02-20 17:06:01 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get("/luogo",(req,resp) =>{
+    resp.send("Ciao!");
+    resp.sendStatus(200);
+})
+
+const listener = app.listen(process.env.PORT, () => {
+    console.log("Your app is listening on port " + listener.address().port);
+  });
