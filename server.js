130579aae8da3d7c089dcb2d45dacedc49c3b275
Francesco <48328557+Francy9@users.noreply.github.com>
2021-02-20 17:45:34 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const app = express();
 const got = require('got');
 var key = "aca959ae6d5053b35c1f3e2f551b0b49";
 
-<<<<<<< HEAD
+
 app.get("/luogo",async(req,resp) =>{
     const città = req.query.città;
     const meteo = await got('api.openweathermap.org/data/2.5/weather?q='+città'&appid='+key);
     const tempo = meteo.body;
     resp.send(tempo);
-=======
+
 app.get("/luogo",(req,resp) =>{
     console.log("richiesta in arrivo!!");
     resp.send("Ciao!");
