98e7557eee5b2b4aebc266bb6b0f449686aefde4
Francesco <franci.ducci@gmail.com>
2021-02-20 18:09:58 +0100

    prova

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var key = "aca959ae6d5053b35c1f3e2f551b0b49";
 
 app.get("/luogo",async(req,resp) =>{
     const luogo = req.query.città;
-    const meteo = await got("api.openweathermap.org/data/2.5/weather?q="+luogo"&appid="+key);
+    const meteo = await got('api.openweathermap.org/data/2.5/weather?q=milano&appid=aca959ae6d5053b35c1f3e2f551b0b49');
     const tempo = meteo.body;
     resp.send(tempo);
     resp.sendStatus(200);
