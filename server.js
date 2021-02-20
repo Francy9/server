96af96bac41f3a9b32eac9f9c40636b044fc701a
Francesco <48328557+Francy9@users.noreply.github.com>
2021-02-21 00:10:03 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,13 +5,14 @@ var key = "aca959ae6d5053b35c1f3e2f551b0b49";
 
 app.get("/luogo",async(req,resp) =>{
    const città = req.query.città;
+   console.log(città);
     try {
-		const response = await got('http://api.openweathermap.org/data/2.5/weather?q='+ città +'&appid='+ key +'&mode=json&units=metric&lang=it');
+	const response = await got('http://api.openweathermap.org/data/2.5/weather?q='+ città +'&appid='+ key +'&mode=json&units=metric&lang=it');
         const corpo = JSON.parse(response.body);
         resp.send(corpo);
 	} catch (error) {
-		console.log(error.response.body);
-    resp.sendStatus(404);
+	console.log(error.response.body);
+    	resp.sendStatus(404);
 		//=> 'Internal server error ...'
 	}
 })
