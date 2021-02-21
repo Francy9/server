ea86e81df77c961c1ef078870fb62c14ec3ae479
Francesco <48328557+Francy9@users.noreply.github.com>
2021-02-21 18:17:48 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ app.get("/luogo/:nome",async(req,resp) =>{
     try {
 	const response = await got('http://api.openweathermap.org/data/2.5/weather?q='+ nome +'&appid='+ key +'&mode=json&units=metric&lang=it');
         const corpo = JSON.parse(response.body);
-        resp.send("temperatura:" + corpo.main.temp);
+        resp.send("temperatura:" + parseInt(corpo.main.temp) +
+             	  "\n minima:" + parseInt(corpo.main.temp_min));
 	} catch (error) {
 	console.log(error.response.body);
     	resp.status(404).end();
