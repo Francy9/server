6db1cc07fe29b7b36a491768d5799c117074acf5
Francesco <48328557+Francy9@users.noreply.github.com>
2021-02-21 22:16:35 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,12 +9,21 @@ app.get("/luogo/:nome",async(req,resp) =>{
     try {
 	const response = await got('http://api.openweathermap.org/data/2.5/weather?q='+ nome +'&appid='+ key +'&mode=json&units=metric&lang=it');
         const corpo = JSON.parse(response.body);
-        resp.send("tempo:" + corpo.weather[1].description +
-		 "\ntemperatura :" + corpo.main.temp +
-		 "\n Minime :" + corpo.main.temp_min + 
-		 "\n Massime :" + corpo.main.temp_max + 
-		 "\n sunrise " + toLocaleString(corpo.sys.sunrise));         
-	console.log("temperatura"+Number.parseInt(corpo.main.temp));  
+        const tempo = JSON.stringify(corpo.weather).split(",")[2].split(":")[1];  
+    	const millisecAlb = corpo.sys.sunrise * 1000;
+    	const dataAlb = new Date(millisecAlb);
+    	const alba =dataAlb.toLocaleString();
+    	const millisecTram = corpo.sys.sunset * 1000;
+    	const dataTram = new Date(millisecTram);
+    	const tramonto =dataTram.toLocaleString();
+    	//console.log(JSON.stringify());
+    	resp.send("Tempo: " + tempo +
+		 "\nTemperatura: " + corpo.main.temp + "°C"+
+     		 "\nPercepita: " + corpo.main.feels_like +"°C" +
+		 "\n Minime: " + corpo.main.temp_min + "°C"+
+		 "\n Massime: " + corpo.main.temp_max + "°C"+
+		 "\n Alba: " + alba +
+     		 "\ntramonto: " + tramonto); 
 	} catch (error) {
 	console.log(error.response.body);
     	resp.status(404).end();
